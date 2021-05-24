7249dc4a46bab61bbc6308512e97bd003359e8d6
Roel Lucassen <roel.lucassen@student.fontys.nl>
2021-05-24 22:17:14 +0200

    Update LocationLogic.cs

--- Whatsdown-Location-Service/Logic/LocationLogic.cs
+++ Whatsdown-Location-Service/Logic/LocationLogic.cs
@@ -12,7 +12,7 @@ namespace Whatsdown_Location_Service.Logic
     public class LocationLogic
     {
 
-
+        //
         public async Task<IpLocation> GetLocationFromIPAsync(string ip)
         {
             var match = Regex.Match(ip, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
@@ -21,16 +21,16 @@ namespace Whatsdown_Location_Service.Logic
                 throw new ArgumentException("The variable is not an ip adress");
             }
 
-                var client = new HttpClient();
+            var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
                 RequestUri = new Uri("https://weatherapi-com.p.rapidapi.com/ip.json?q=" + ip),
                 Headers =
-    {
+             {
         { "x-rapidapi-key", "af62e88497msh6cf846cb65ee11fp19a3e4jsn459a722086e1" },
         { "x-rapidapi-host", "weatherapi-com.p.rapidapi.com" },
-    },
+             },
             };
             using (var response = await client.SendAsync(request))
             {
@@ -38,8 +38,6 @@ namespace Whatsdown_Location_Service.Logic
                 {
                     var body = await response.Content.ReadAsStringAsync();
                     IpLocation location = JsonConvert.DeserializeObject<IpLocation>(body);
-                    Console.WriteLine(location);
-                    Console.WriteLine(location);
                     return location;
                 }
             }
